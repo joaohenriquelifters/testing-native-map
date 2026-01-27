282e1414c065bb1444fc267ba47d1c4b3435f81d
joaohenriquelifters <joao.henrique@twinfo.io>
2026-01-27 09:32:55 -0300

    custom theme colors

--- test-map/app/(tabs)/map.tsx
+++ test-map/app/(tabs)/map.tsx
@@ -3,85 +3,101 @@ import { StyleSheet, View, Dimensions } from "react-native";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 
 const mapStyle = [
-    { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-    { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-    { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-    {
-        featureType: "administrative.locality",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
-    },
-    {
-        featureType: "poi",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
-    },
-    {
-        featureType: "poi.park",
-        elementType: "geometry",
-        stylers: [{ color: "#00e1ffff" }],
-    },
-    {
-        featureType: "poi.park",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#6b9a76" }],
-    },
-    {
-        featureType: "road",
-        elementType: "geometry",
-        stylers: [{ color: "#00ffaaff" }],
-    },
-    {
-        featureType: "road",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#ff0000ff" }],
-    },
-    {
-        featureType: "road",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#0062ffff" }],
-    },
-    {
-        featureType: "road.highway",
-        elementType: "geometry",
-        stylers: [{ color: "#746855" }],
-    },
-    {
-        featureType: "road.highway",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#1f2835" }],
-    },
-    {
-        featureType: "road.highway",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#f3d19c" }],
-    },
-    {
-        featureType: "transit",
-        elementType: "geometry",
-        stylers: [{ color: "#2f3948" }],
-    },
-    {
-        featureType: "transit.station",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
-    },
-    {
-        featureType: "water",
-        elementType: "geometry",
-        stylers: [{ color: "#17263c" }],
-    },
-    {
-        featureType: "water",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#515c6d" }],
-    },
-    {
-        featureType: "water",
-        elementType: "labels.text.stroke",
-        stylers: [{ color: "#17263c" }],
-    },
+  // Fundo geral
+  { elementType: "geometry", stylers: [{ color: "#121212" }] },
+  { elementType: "labels.text.stroke", stylers: [{ color: "#121212" }] },
+  { elementType: "labels.text.fill", stylers: [{ color: "#B3B3B3" }] },
+
+  // Cidades / nomes principais
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#FFFFFF" }],
+  },
+
+  // Pontos de interesse (lugares)
+  {
+    featureType: "poi",
+    elementType: "geometry",
+    stylers: [{ color: "#181818" }],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#1DB954" }], // verde Spotify
+  },
+
+  // Parques
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [{ color: "#1DB95433" }], // verde com transparência
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#1DB954" }],
+  },
+
+  // Estradas
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [{ color: "#282828" }],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#1DB954" }],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#B3B3B3" }],
+  },
+
+  // Rodovias principais
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [{ color: "#1DB954" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#FFFFFF" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#121212" }],
+  },
+
+  // Água
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [{ color: "#181818" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#1DB954" }],
+  },
+
+  // Transporte público
+  {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [{ color: "#282828" }],
+  },
+  {
+    featureType: "transit.station",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#1DB954" }],
+  },
 ];
+
 
 export default function MapaScreen() {
     return (
